c9192331c76803cd85aab98872a05ffe1126167c
Jason Elter <jason.elter@cs.huji.ac.il>
2021-03-22 21:27:17 +0200

    First commit

---
+++ app/src/main/java/huji/jasonelter/postpc_2021_ex0/MainActivity.java
@@ -0,0 +1,14 @@
+package huji.jasonelter.postpc_2021_ex0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
